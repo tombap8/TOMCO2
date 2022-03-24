f14e1ae4a550039afcaab076341578a9ef0de4c2
Tom B. Parker <tombap8@hotmail.com>
2022-03-24 16:18:28 +0900

    ss

--- 001.HTML학습/07.CGV_PJ/003.site/js/main.js
+++ 001.HTML학습/07.CGV_PJ/003.site/js/main.js
@@ -16,4 +16,5 @@ function chgMV(mvid){ // mvid - 영화아이디값 전달변수
     // src중 영화 아이디부분을 변경함!
     tg.src = `https://www.youtube.com/embed/${mvid}?autoplay=1&playsinline=1`;
 
+
 } ////////// chgMV 함수 ////////////////////
