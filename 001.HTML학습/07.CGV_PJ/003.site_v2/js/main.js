48aae4eaeb27990a086117388556ff920f08eab2
Tom B. Parker <tombap8@hotmail.com>
2022-03-31 15:50:30 +0900

    Update main.js

--- 001.HTML학습/07.CGV_PJ/003.site_v2/js/main.js
+++ 001.HTML학습/07.CGV_PJ/003.site_v2/js/main.js
@@ -18,3 +18,36 @@ function chgMV(mvid){ // mvid - 영화아이디값 전달변수
 
 
 } ////////// chgMV 함수 ////////////////////
+
+
+/////// 로딩구역 ///////////////////////////
+window.addEventListener("load",() => {
+
+    console.log("로딩완료!");
+
+    // 포스터 메뉴 li 클릭시 li에 클래스 on넣기
+    // 대상: .mlist ul>li
+    let mlist = document
+    .querySelectorAll(".mlist ul>li");
+
+    console.log("리스트개수:",mlist.length);
+
+    // 클래스 초기화 함수 ///
+    const resetFn = ()=>{
+        for(let x of mlist) 
+            x.classList.remove("on");
+    }; ////// resetFn함수 //////////
+
+    // 대상만큼 click이벤트 설정하기
+    // for of 사용!
+    for(let x of mlist){
+        x.onclick = ()=>{
+            // 1. 클래스 초기화함수 호출
+            resetFn();
+            // 2. 해당li요소 클래스 on넣기
+            x.classList.add("on");
+        }; ///////// click /////////
+    } ///////// for of ////////////////
+
+
+}); /////////// 로딩구역 /////////////////////
