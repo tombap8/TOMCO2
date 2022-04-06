246d0a63a1ab2d3c9114ac3927b2ed8884b3036c
Tom B. Parker <tombap8@hotmail.com>
2022-04-06 16:52:59 +0900

    ss

---
+++ 001.HTML학습/08.VOGUE_PJ/002.site/js/common.js
@@ -0,0 +1,8 @@
+// 보그 PJ 공통 JS - common.js
+
+///////////////// 로드구역 ///////////////////////
+window.addEventListener("DOMContentLoaded",()=>{
+
+    console.log("로딩완료");
+    
+}); //////////// 로드구역 ////////////////////////
