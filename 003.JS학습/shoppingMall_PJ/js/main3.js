2be00a8765bad11b11831722e710c74fe3c16ff1
Tom B. Parker <tombap8@hotmail.com>
2022-04-07 11:04:02 +0900

    Update main3.js

--- 003.JS학습/shoppingMall_PJ/js/main3.js
+++ 003.JS학습/shoppingMall_PJ/js/main3.js
@@ -52,7 +52,10 @@ window.addEventListener("load", () => {
         함수명: goSlide
         기능: 슬라이드를 왼쪽/오른쪽 이동함
     **************************************/
-    const goSlide = dir => { // dir - 이동방향(1:오른쪽,0:왼쪽)
+    const goSlide = (dir, gb) => {
+        // dir - 이동방향(1:오른쪽,0:왼쪽)
+        // gb - 구분코드(인터발호출일때만 값이 전달됨)
+
 
         // console.log("잠금상태:",prot);
 
@@ -63,7 +66,11 @@ window.addEventListener("load", () => {
         // 0.6초후 잠금해제! //////
 
         // 1. 전달값 및 호출확인
-        console.log("이동!", dir);
+        console.log("이동!", dir, gb);
+
+        // 1.2. 버튼클릭시 gb전달값이 없는 경우
+        // !gb 는 undefined인 경우 false임 -> true처리
+        if (!gb) clearAuto(); // 인터발함수 지우기!
 
 
         // 2. 방향분기   /////////////
@@ -73,7 +80,7 @@ window.addEventListener("load", () => {
 
             // (1) 슬라이드 li 순번 증가하기
             sno++;
-            if(sno===lmt) sno = 0; 
+            if (sno === lmt) sno = 0;
             // 마지막번호 넘을시 처음으로!
 
         } //////////// if ///////////
@@ -83,14 +90,14 @@ window.addEventListener("load", () => {
 
             // (1) 슬라이드 li 순번 증가하기
             sno--;
-            if(sno===-1) sno = lmt-1; 
+            if (sno === -1) sno = lmt - 1;
             // 첫번호 이전일때 마지막번호로!
             // 마지막 번호는 개수-1
 
         } /////////// else //////////
 
         // (2) 슬라이드li 초기화
-        for(let x of sli) x.classList.remove("on");
+        for (let x of sli) x.classList.remove("on");
 
         // (3) 해당번호 li에 클래서 on넣기
         sli[sno].classList.add("on");
@@ -112,6 +119,51 @@ window.addEventListener("load", () => {
     // 왼쪽버튼 클릭시
     abtn[0].onclick = () => goSlide(0);
 
+    /******************************************** 
+        [ 인터발 자동호출의 조건 ]
+        1. 일정시간 간격으로 슬라이드가 넘어감
+        2. 사용자가 버튼조작을 할경우 자동멈춤
+        3. 일정시간 버튼조작이 없으면 다시자동
+    ********************************************/
+
+    // 인터발용변수
+    let autoI;
+
+    ////////////////////
+    // 인터발 호출함수 //
+    ////////////////////
+    const autoCall = () => {
+        // 인터발 자동호출!
+        autoI = setInterval(() => goSlide(1, 1), 2000);
+        // 지우기 위해 변수에 할당함!
+
+    }; /////// autoCall함수 ///////////
+
+    // 인터발 호출함수 최초호출!
+    autoCall();
+
+    // 타임아웃용 변수
+    let autoT;
+
+    ////////////////////
+    // 인터발 삭제함수 //
+    ////////////////////
+    // -> 슬라이드 이동버튼 클릭시 호출됨!
+    const clearAuto = () => {
+
+        console.log("인터발지우기!");
+
+        // 1. 인터발지우기 + 타임아웃지우기
+        clearInterval(autoI);
+        clearTimeout(autoT);
+        // 한번씩 셋팅되는 타임아웃을 안지우면
+        // 여러개가 작동하여 실행쓰나미가 발행함!
+
+        // 2. 일정시간후 다시 인터발호출하기!
+        autoT = setTimeout(autoCall, 4000);
+        // 4초후 autoCall()함수 호출!
+
+    }; ////// clearAuto함수 ///////////
 
 
 }); ///////////// load //////////////////////
