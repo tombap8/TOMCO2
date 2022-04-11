e7874491d21e71668795380eaf0423c17573330a
Tom B. Parker <tombap8@hotmail.com>
2022-04-11 16:19:37 +0900

    Update linksys.js

--- 001.HTML학습/08.VOGUE_PJ/002.site/js/linksys.js
+++ 001.HTML학습/08.VOGUE_PJ/002.site/js/linksys.js
@@ -7,8 +7,10 @@ $(()=>{ //////////// jQB /////////////////////////
     // GNB메뉴 a요소 클릭시 링크 연결하기
 
     $(".gnb a").click(function(){
-        let txt = $(this).text().trim();
+        let txt = $(this).text().trim().toLowerCase();
         // trim() - 앞뒤공백 제거!
+        // toLowerCase() - 소문자로 변환!
+        // 참고) toUpperCase() - 대문자로 변환
         console.log("메뉴글자:",txt);
     }); /////////// click /////////////
 
