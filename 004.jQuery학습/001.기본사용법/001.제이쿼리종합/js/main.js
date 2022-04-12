f9bcce1e20e0c50edf033fb353f9f5fcc69f8977
Tom B. Parker <tombap8@hotmail.com>
2022-04-12 10:21:47 +0900

    Update main.js

--- 004.jQuery학습/001.기본사용법/001.제이쿼리종합/js/main.js
+++ 004.jQuery학습/001.기본사용법/001.제이쿼리종합/js/main.js
@@ -1,16 +1,28 @@
-const { default: $ } = require("../../../004.플러그인/009.swiper-4.1.0/swiper-4.1.0/src/utils/dom");
-
 // 제이쿼리 기본 JS - main.js 
-$(()=>{ ///////// jQB /////////////////////////
+$(() => { ///////// jQB /////////////////////////
 
     console.log("로딩완료!");
 
     /**************************************** 
                 1. 대상선정 변수할당
     ****************************************/
-   // 대상1 : 버튼들 - .btns>button
-   let btns = $(".btns>button");
-   console.log("버튼개수:",btns.length);
+    // 대상1 : 버튼들 - .btns>button
+    let btns = $(".btns>button");
+    //    console.log("버튼개수:",btns.length);
 
-   
+    // 대상2 : 미니언즈 - .mi
+    let mi = $(".mi");
+
+    // 대상3 : 빌딩각방 - .building li
+    let bd = $(".building li");
+    //    console.log("방수:",bd.length);
+
+    // 대상4 : 메시지 박스 - .msg
+    let msg = $(".msg");
+
+    // 삽입이미지 변수 셋팅 //////
+    // 좀비 이미지 태그
+    let mz1 = 
+        ``;
+
 }); ///////////// jQB /////////////////////////
