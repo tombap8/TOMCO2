021bd23587e07a272c91647f71dd8a37b4e69d8f
Tom B. Parker <tombap8@hotmail.com>
2022-04-15 10:15:07 +0900

    ss

--- 001.HTML학습/08.VOGUE_PJ/002.site/js/gallery.js
+++ 001.HTML학습/08.VOGUE_PJ/002.site/js/gallery.js
@@ -5,11 +5,11 @@ $(()=>{//////////// jQB //////////////////////////////
     console.log("로딩완료!");
     
     var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 3,
-        spaceBetween: 30,
-        slidesPerGroup: 3,
-        loop: true,
-        loopFillGroupWithBlank: true,
+        slidesPerView: 3, // 한 화면당 슬라이드 개수
+        spaceBetween: 30, // 슬라이드 사이간격(px)
+        slidesPerGroup: 1, // 슬라이드 묶음 개수(넘기는단위)
+        loop: true, // 무한루프(기본값false)
+        // loopFillGroupWithBlank: true,
         pagination: {
           el: ".swiper-pagination",
           clickable: true,
