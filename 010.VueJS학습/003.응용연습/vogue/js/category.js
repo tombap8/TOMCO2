bbd6aed2b40756dbc166b4b253ce222498838b4a
Tom B. Parker <tombap8@hotmail.com>
2022-04-20 12:20:24 +0900

    Update category.js

--- 010.VueJS학습/003.응용연습/vogue/js/category.js
+++ 010.VueJS학습/003.응용연습/vogue/js/category.js
@@ -20,7 +20,7 @@ window.addEventListener("DOMContentLoaded", () => {
     console.log("로딩완료");
 
     //// 데이터 바인딩 Vue 인스턴스 생성하기 ///
-    new Vue({
+    let db = new Vue({
         el: "#cont", 
         // 바인딩할 대상(변경요소를 포함하는 부모요소)->아이디만 적용됨!
         data: {
@@ -44,6 +44,36 @@ window.addEventListener("DOMContentLoaded", () => {
         } ///////// methods //////////
 
     }); ////////////// Vue //////////////
+
+    /////// 뷰JS 체험존 GNB메뉴 클릭시 셋팅 /////
+    $(".VueGnb a").click(function(e){
+
+        // 기본기능막기
+        e.preventDefault();
+
+        let txt = $(this).text().toLowerCase();
+        // toLowerCase() 소문자변환
+        console.log("체험존:",txt);
+
+        // 파라미터 변수값 업데이트
+        pm = txt;
+        console.log("체험존pm업뎃:",pm);
+        // pm 값이 뷰JS 인스턴스안에 catName 변수로
+        // 셋팅되어 있지만 이 값을 업데이트 해도
+        // 페이지가 업데이트 되지 않는다!
+        // 왜? 이것은 뷰JS의 변수가 아니기 때문!!!
+
+        // 뷰JS의 대표특징인 양방향바인딩 즉,
+        // 뷰JS를 업데이트하면 화면이 업데이트되는 기능!
+        // 뷰JS의 데이터인 catName을 업데이트 하면된다!!!
+
+        // 뷰JS의 변수 접근법:
+        // 뷰JS 인스턴스를 변수에 담는다 -> db
+        // 변수.$data.변수명 = 값
+        db.$data.catName = txt;
+
+
+    }); /////////// click ///////////////
 
 
 }); ///////////// 로딩구역 ////////////////////////
